0b24a89c4081f33258a016fb30695f092f681da8
Songkor95 <darlings.world1995@gmail.com>
2025-09-28 05:15:09 +0400

    adding new questions

--- ask_name.py
+++ ask_name.py
@@ -1,3 +1,5 @@
-ask = input("What is your name?: ").title()
+ask_name = input("What is your name?: ").title()
+ask_age = int(input("What's your age?: "))
 
-print(f"My name is {ask}.")
+
+print(f"My name is {ask_name} and I am {ask_age} years old.")
