454fda72f8c431533881c5bcac74b27145d1ed94
Songkor95 <darlings.world1995@gmail.com>
2025-09-30 04:07:44 +0400

    romove one question

--- ask_name.py
+++ ask_name.py
@@ -1,6 +1,5 @@
 ask_name = input("What is your name?: ").title()
 ask_age = int(input("What's your age?: "))
 ask_homecountry = input("Where are you from?: ").title()
-ask_plan = input("What is your future plan?: ").capitalize()
 
-print(f"Hi, My name is {ask_name} and I am {ask_age} years old. I am from {ask_homecountry}. It is very beautiful country. I am planning to go back to my home and start small business.")
+print(f"Hi, My name is {ask_name} and I am {ask_age} years old. I am from {ask_homecountry}. It is very beautiful landlock country.")
